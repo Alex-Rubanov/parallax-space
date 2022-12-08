4ca1ce78985299fc32f8e437649884b8c749c882
Alex <rubanovv.alex@gmail.com>
2022-12-08 19:24:27 +0100

    parallax effect on mousemove

---
+++ script.js
@@ -0,0 +1,12 @@
+function parallax(e) {
+    document.querySelectorAll('.space').forEach(el => {
+        let movingValue = el.getAttribute('data-value');
+        console.log(e.clientX, e.clientY);
+        let x = e.clientX * movingValue /150;
+        let y = e.clientY * movingValue /150;
+
+        el.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
+document.addEventListener('mousemove', parallax);
